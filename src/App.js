899413ff84ac10d804d940578cab82f32baeea56
seohyeon1578 <seohyeon1578@naver.com>
2022-05-07 21:39:34 +0900

    fix 3D model

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import "./style.css";
-import React from "react";
+import React, { Suspense}  from "react";
 import { Canvas } from 'react-three-fiber';
 import { OrbitControls} from "@react-three/drei";
-import { Physics } from '@react-three/cannon';
 import { Jodan } from "./Pages/PageList";
 
 export default function App() {
   return (
-    <Canvas>
-      <OrbitControls />
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 15, 10]} angle={0.3} />
-      <Physics>
-        <Jodan />
-      </Physics>
-    </Canvas>
+    <>
+      <Canvas>
+        <OrbitControls />
+        <ambientLight intensity={0.3} />
+        <spotLight intensity={0.3} position={[5, 25, 20]} angle={0.1} penumbra={1} />
+        <Suspense fallback={null}>
+          <Jodan />
+        </Suspense>
+      </Canvas>
+    </>
   )
 }
