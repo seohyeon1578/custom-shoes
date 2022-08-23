0509c188032de94740452dad4d2f1b4351d6770b
seohyeon1578 <seohyeon1578@naver.com>
2022-08-23 16:04:52 +0900

    all picker

--- src/components/picker/AllPicker.js
+++ src/components/picker/AllPicker.js
@@ -1,7 +1,15 @@
+import { HexColorPicker } from "react-colorful";
+import { useSnapshot  } from "valtio";
+import { state } from "../../state/State";
+
 export const AllPicker = () => {
+  const snap = useSnapshot(state);
   return(
     <div style={{display: "flex"}}>
-      
+      <HexColorPicker 
+        color={snap.items[snap.current]}
+        onChange={color => state.items[snap.current] = color}
+      />
     </div>
   );
 };
